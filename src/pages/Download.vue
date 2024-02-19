050e89511feb81ae0d9901ce429c7743bbc65dae
Mostafa Kazemi <mostafa.kazeminejhad@gmail.com>
2024-02-19 22:04:53 +0330

    Create solid styling

--- src/pages/Download.vue
+++ src/pages/Download.vue
@@ -2,17 +2,17 @@
   <div class="flex flex-col gap-y-32">
     <DownloadHeroSection />
     <AppsSection />
-    <FeaturesSection />
+    <FeaturesSectionSolid />
   </div>
 </template>
 
 <script>
 import DownloadHeroSection from '@/components/Download/HeroSection/index.vue'
 import AppsSection from '@/components/Download/AppsSection/index.vue'
-import FeaturesSection from '@/components/FeaturesSection/index.vue'
+import FeaturesSectionSolid from "@/components/FeaturesSection/Solid/index.vue";
 
 export default {
   name: 'DownloadPage',
-  components: { FeaturesSection, AppsSection, DownloadHeroSection }
+  components: {FeaturesSectionSolid, AppsSection, DownloadHeroSection }
 }
 </script>
